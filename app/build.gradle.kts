ec8e9a10b613643efb0936905578fe6b14530dd8
Secozzi <folke.steen85@gmail.com>
2025-06-09 18:37:30 +0200

    Minify debug builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,7 +78,7 @@ android {
     }
 
     buildTypes {
-        release {
+        all {
             isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
